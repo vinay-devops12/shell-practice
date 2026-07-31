43d6e5605e1efc18834c7f3df0c15166f4ebe026
vinay-devops12 <vinaypayyavula9@gmail.com>
2026-07-31 14:40:56 +0530

    shell

--- 06-special.variable.sh
+++ 06-special.variable.sh
@@ -6,4 +6,4 @@ echo " Number of variables passed:$#"
 echo " script name:$0"
 echo "who is running this: $USER"
 echo "which directort :$PWD"
-echo " PID  of current script:$$
+echo " PID  of current script:$$"
