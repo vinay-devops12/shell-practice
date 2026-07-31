ee852f9060b5724926ce8771f031b71a8d9c6709
vinay-devops12 <vinaypayyavula9@gmail.com>
2026-07-31 17:06:57 +0530

    shell

--- 09-install.sh
+++ 09-install.sh
@@ -3,8 +3,8 @@
 ## any install coomand   user should be root access
 
 USERID=$( id -u )
- if [$ USERID -ne 0 ]
-  echo " please run this script with root access"
-  fi 
+ if [ $USERID -ne 0 ] ; then
+     echo  "please run this script with root access"
+  fi
 
-   echo " iam continuing ...
+   echo " iam continuing ..."
