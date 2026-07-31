09be4476c734edd6d5383b903e231fe71cf8e133
vinay-devops12 <vinaypayyavula9@gmail.com>
2026-07-31 10:06:15 +0530

    conversations

---
+++ 03-variable.sh
@@ -0,0 +1,14 @@
+
+person1=$1
+person2=$2
+
+
+echo " $persion1: we are going school"
+echo "$persion2 : you will go $persion1"
+echo " if your camepersion2 iam waiting"
+echo " $persion1 you will go iam coming later"
+echo "$persion2 any work"
+echo " iam going temple $persion1"
+echo "how much time take $persion2"
+echo " $persion1 more then one hours  i will come "
+echo "$persion2 iam waiting for you "
