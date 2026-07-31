3f2b8c3c459aa4bbf2d147fe31367afc63c0ab02
vinay-devops12 <vinaypayyavula9@gmail.com>
2026-07-31 15:24:36 +0530

    shell

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -7,12 +7,15 @@ SUM=$(($NUM1+$NUM2))
 echo " sum: $SUM"
 
 
-
-
-
 NUM1=10
 NUM2=linux
 SUM=$(($NUM1+$NUM2))
 
 echo " sum: $SUM"
+
+### arrey command 
+## position start with (zero).. and numbering start with (one..)
+
+MOVIES = ("RRR" "VARANASI" "KING")
+echo " movie are : ${MOVIES[@]}"
 
