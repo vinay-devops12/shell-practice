d5e854b947eab6241c15faa6f1def7823f8a04a4
vinay-devops12 <vinaypayyavula9@gmail.com>
2026-07-31 09:31:46 +0530

    conversations

---
+++ 02-conversion.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo " raju: we are going school"
+echo " puri : you will go  raju"
+echo " if your came puri iam waiting"
+echo " raju you will go iam coming later"
+echo " puri any work"
+echo " iam going temple raju"
+echo "how much time take puri"
+echo " raju more then one hours  i will come "
+echo " puri iam waiting for you "
+
+
+
