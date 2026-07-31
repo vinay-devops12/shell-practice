b1fd4f813d301ab0241a2c7a4c30f3cd18507aa8
vinay-devops12 <vinaypayyavula9@gmail.com>
2026-07-31 19:42:21 +0530

    shell

--- 09-install.sh
+++ 09-install.sh
@@ -2,9 +2,18 @@
 
 ## any install coomand   user should be root access
 
-USERID=$( id -u )
- if [ $USERID -ne 0 ] ; then
-     echo  "please run this script with root access"
-  fi
+# USERID=$( id -u )
+#  if [ $USERID -ne 0 ] ; then
+#      echo  "please run this script with root access"
+#   fi
 
-   echo " iam continuing ..."
+#    echo " iam continuing ..."
+
+
+if [ $? -ne 0 ]; then
+ echo "install mysql is ...failled"
+
+else
+ echo "install mysql is ..success"
+ fi
+
