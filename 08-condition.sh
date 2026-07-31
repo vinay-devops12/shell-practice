c741ae1d19a03f66b27708a38828187b3ef386eb
vinay-devops12 <vinaypayyavula9@gmail.com>
2026-07-31 16:40:41 +0530

    shell

--- 08-condition.sh
+++ 08-condition.sh
@@ -5,3 +5,9 @@
 if [ $NUMBER -ge 20 ]; then
    echo "given number $NUMBER is greather then or equal to 20"
 fi
+
+
+
+if [ $NUMBER -le 20 ]; then
+   echo "given number $NUMBER is less then or equal to 20"
+fi
