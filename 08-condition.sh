badc19fb7ac31740e66b83baf758ed72e5c02734
vinay-devops12 <vinaypayyavula9@gmail.com>
2026-07-31 16:38:53 +0530

    shell

--- 08-condition.sh
+++ 08-condition.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-NUMBER=$1
+    NUMBER=$1
 
- if [$NUMBER -ge 20]; then
- echo " given number $NUMBER is greather then or equal to 20
- fi
+if [ $NUMBER -ge 20 ]; then
+   echo "given number $NUMBER is greather then or equal to 20"
+fi
