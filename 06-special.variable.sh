6525240df071ac202c0622cc3c0491874e16007c
vinay-devops12 <vinaypayyavula9@gmail.com>
2026-09-02 20:55:48 +0530

    Update changes

--- 06-special.variable.sh
+++ 06-special.variable.sh
@@ -4,7 +4,7 @@
 echo "All variables passed to script: $@"
 echo " Number of variables passed:$#"
 echo " script name:$0"
-echo "who is running this: $USER"
+echo "who is runningg this: $USER"
 echo "which directort :$PWD"
 echo " PID  of current script:$$"
 echo " pid of the back ground command running just now:$!"
